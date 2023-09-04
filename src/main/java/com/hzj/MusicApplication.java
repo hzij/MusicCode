42f3db3de677063f3c32927ba6176a492abb6565
郝紫俊 <hzjun20021001@163.com>
2023-09-04 20:53:58 +0800

    登录功能：没有添加过滤器，可以通过地址访问其他页面

---
+++ src/main/java/com/hzj/MusicApplication.java
@@ -0,0 +1,19 @@
+package com.hzj;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, SecurityFilterAutoConfiguration.class})
+@Slf4j
+public class MusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicApplication.class, args);
+        log.info("项目启动成功");
+    }
+
+}
